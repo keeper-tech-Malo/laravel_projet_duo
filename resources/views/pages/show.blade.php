a89b65100659caa2736d1b310a57706bb151575f
Malo <ndekezi37@gmail.com>
2021-04-21 16:35:03 +0200

    fin du travail

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>show</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link href={{asset("css/bootstrap.min.css")}} rel="stylesheet">
 </head>
 <body>
     <table class="table table-bordered container">
